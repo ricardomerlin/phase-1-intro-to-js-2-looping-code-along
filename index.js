00ac8c9e5c66b65afada665284cfc64a84a64634
Ricardo Merlin <rcrdmerlin@gmail.com>
2023-11-02 21:43:17 -0400

    uploading looping lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+    let messages = [];
+    for (let i=0; i<names.length; i++) {
+        let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+        messages.push(message);
+    }
+    return messages;
+}
+
+
+let num = Number
+
+function countDown(num) {
+  for (let i = num; i > -1; i--) {
+    console.log(i);
+  }
+}
+
+console.log(countDown())
